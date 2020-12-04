80130f9a574569fb203f891c4a7c3382b434ddd2
Soumajit Choudhuri <soumajitchoudhuri@gmail.com>
2020-12-04 11:48:47 +0530

    added edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 import { Todo } from './todo';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+
+  // animations: [
+  //   // the fade-in/fade-out animation.
+  //   trigger('simpleFadeAnimation', [
+  //     // the "in" style determines the "resting" state of the element when it is visible.
+  //     state('in', style({ opacity: 1 })),
+
+  //     // fade in when created. this could also be written as transition('void => *')
+  //     transition(':enter', [style({ opacity: 0 }), animate(200)]),
+
+  //     // fade out when destroyed. this could also be written as transition('void => *')
+  //     transition(':leave', animate(400, style({ opacity: 0 }))),
+  //   ]),
+  // ],
 })
 export class AppComponent {
   title = 'Project CC';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
